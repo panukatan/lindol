c021cfe9ed080aaf2095cb305140dbb86924aa85
Ernest Guevarra <ernest@guevarra.io>
2024-08-18 17:33:59 +0100

    add tests for eq_get_links

--- tests/testthat/test-eq_get.R
+++ tests/testthat/test-eq_get.R
@@ -45,21 +45,12 @@ test_that("tables are of the correct structure", {
 
 tab_list <- eq_get_links()
 
-test_that("get table function outputs are as expected", {
-  expect_type(tab_list, "list")
+test_that("get_links function outputs are as expected", {
+  expect_vector(tab_list, ptype = character(), size = nrow(eq_data()))
 })
+
+tab_list <- eq_get_links(.year = 2018)
 
 test_that("tables are of the correct structure", {
-  expect_vector(tab_list, size = 1)
-})
-
-tab_list <- eq_get_links(.year = 2020)
-
-test_that("get table function outputs are as expected", {
-  expect_type(tab_list, "list")
-})
-
-test_that("tables are of the correct structure", {
-  expect_vector(tab_list, size = 12)
-  expect_named(tab_list, expected = paste(month.name, 2020))
+  expect_vector(tab_list, ptype = character())
 })
