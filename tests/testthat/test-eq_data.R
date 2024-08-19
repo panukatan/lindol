fe286b28371dd287da1bb702bdba121e51370815
Ernest Guevarra <ernest@guevarra.io>
2024-08-19 12:46:49 +0100

    add tests for NULL tables/urls

--- tests/testthat/test-eq_data.R
+++ tests/testthat/test-eq_data.R
@@ -4,9 +4,9 @@ test_that("eq_data function output is appropriate", {
   expect_s3_class(eq_data_summary(), "tbl")
 })
 
-#eq_df <- eq_data_bulletin(.year = 2018, .month = "January")
+eq_df <- eq_data_bulletin(.year = 2018, .month = "January")
 #eq_df <- eq_data_bulletin(.year = 2019, .month = "January")
 
-# test_that("eq_data function output is appropriate", {
-#   expect_s3_class(eq_df, "tbl")
-# })
+test_that("eq_data function output is appropriate", {
+  expect_s3_class(eq_df, "tbl")
+})
--- tests/testthat/test-eq_process.R
+++ tests/testthat/test-eq_process.R
@@ -36,3 +36,20 @@ test_that("eq_process outputs are appropriate", {
     )
   )
 })
+
+df <- eq_get_tables() |>
+  c(list(NULL)) |>
+  eq_process_tables()
+
+test_that("eq_process outputs are appropriate", {
+  expect_s3_class(df, "tbl")
+  expect_named(
+    df,
+    expected = c(
+      "date_time_retrieved", "date_time",
+      "latitude", "longitude",
+      "depth", "magnitude", "location"
+    )
+  )
+})
+
